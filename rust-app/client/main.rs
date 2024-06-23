312d6cd78b38db2c2d27e3e73ec342c8093c33c4
b17w1z4rd <74816174+b17w1z4rd@users.noreply.github.com>
2024-06-23 12:03:26 +0530

    Add files via upload

--- rust-app/client/main.rs
+++ rust-app/client/main.rs
@@ -1,1 +1,29 @@
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use tokio;
 
+#[derive(Serialize, Deserialize)]
+struct MyData {
+    name: String,
+    age: u32,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+    let data = MyData {
+        name: "Alice".into(),
+        age: 30,
+    };
+
+    let res = client.post("http://127.0.0.1:8080/greet")
+        .json(&data)
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("Response: {}", res);
+
+    Ok(())
+}
